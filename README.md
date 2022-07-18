83f1602f75d93a6f332a2bafd7a565afb3b59ce2
Electropompa <admin@zilmet.ru>
2022-07-18 16:01:53 +0300

    fix gulpfile, add adaptive cols paddings

---
+++ README.md
@@ -0,0 +1,38 @@
+###RESET
+
+=reset-buttons
+
+###COLORS
+Нужно изменить предустановленные цвета для темы в файле _colors.sass:
+
+- accent
+
+Есть стандартные предустановленные цвета:
+
+- white
+- 
+
+### GRID
+For .col-* classes Left & Right padding:
+
+@media (min-width: 0) -> : 10px;
+
+@media (min-width: 576px) -> : 12px;
+
+@media (min-width: 768px) -> : 15px;
+
+@media (min-width: 1024px) -> : 16px;
+
+@media (min-width: 1400px) -> : 17px;
+
+@media (min-width: 1500px) -> : 18px;
+
+###UTILITIES
+
+.white-bg-wrapper
+
+.bg-black
+
+.bg-white
+
+.bg-accent
--- gulpfile.js
+++ gulpfile.js
@@ -3,51 +3,26 @@ const { src, dest, parallel, series, watch } = require('gulp'),
     sass            = require('gulp-sass'),
     autoprefixer    = require('gulp-autoprefixer'),
     cssnano         = require('gulp-cssnano'),
-    pug             = require('gulp-pug'),
     sourcemaps      = require('gulp-sourcemaps'),
     livereload      = require('gulp-livereload');
 
 
-function buildHtml(){
-    return src('**/*.pug')
-        .pipe(pug({ pretty: true }))
-        .pipe(dest(function(file){
-            file.basename = file.basename.split('.')[0] + '.php';
-            return file.base;
-        }))
-        .pipe(livereload())
-}
-exports.buildHtml = buildHtml;
-
 function styles(){
     return src( "components/vars/styles.sass")
-       .pipe(sourcemaps.init())
+       // .pipe(sourcemaps.init())
        .pipe(sass())
        .pipe(autoprefixer())
-       .pipe(cssnano())
-       .pipe(sourcemaps.write())
+       // .pipe(cssnano())
+       // .pipe(sourcemaps.write())
        .pipe(dest("components/"))
        .pipe(livereload())
 }
 exports.styles = styles;
 
-function scripts() {
-    return src([
-        '../node_modules/jquery/dist/jquery.min.js',
-        '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
-        './*/*.js',
-    ])
-    .pipe(concat('script.min.js'))
-    .pipe(dest('build/js/'))
-    .pipe(livereload());
-}
-exports.scripts = scripts;
-
 function startWatch() {
     livereload.listen();
-    watch( ['./*/*.pug', './*/**/*.pug'], buildHtml );
     watch( '**/*.php' ).on('change', livereload.changed);
-    // watch( './*/*.js', scripts );
+    watch( '**/*.html' ).on('change', livereload.changed);
     watch( ['./*/*.sass', './*/**/*.sass'], styles );
 }
 exports.default = startWatch;
