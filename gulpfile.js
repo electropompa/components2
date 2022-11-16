7bbee6bfaf15db5449dcfde60b2a7bbdea39b29d
Electropompa <admin@zilmet.ru>
2022-11-16 17:57:59 +0300

    documentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,14 @@
 const { src, dest, watch } = require('gulp'),
-    concat          = require('gulp-concat'),
     sass            = require('gulp-sass')(require('sass')),
-    autoprefixer    = require('gulp-autoprefixer'),
-    cssnano         = require('gulp-cssnano'),
     sourcemaps      = require('gulp-sourcemaps'),
     livereload      = require('gulp-livereload');
 
 
 function styles(){
     return src( "components/vars/styles.sass")
-       .pipe(sourcemaps.init())
+       // .pipe(sourcemaps.init())
        .pipe(sass())
-       .pipe(autoprefixer())
-       // .pipe(cssnano())
-       .pipe(sourcemaps.write())
+       // .pipe(sourcemaps.write())
        .pipe(dest("components/"))
        .pipe(livereload())
 }
