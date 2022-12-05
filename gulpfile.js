fc26b1e8cf4281f5aea2a6787c2200b0d8d0c984
Electropompa <admin@zilmet.ru>
2022-12-05 11:14:53 +0300

    feat: gulpfile add productionStyles task with postcss, group css media queries, csso

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,37 @@
-const { src, dest, watch } = require('gulp'),
-    sass            = require('gulp-sass')(require('sass')),
-    sourcemaps      = require('gulp-sourcemaps'),
-    livereload      = require('gulp-livereload');
+const { src, dest, watch } = require( 'gulp' ),
+    sass            = require( 'gulp-sass' )(require('sass')),
+    csso            = require( 'gulp-csso' ),
+    autoprefixer    = require('autoprefixer'),
+    postcss         = require( 'gulp-postcss' ),
+    gcmq            = require( 'gulp-group-css-media-queries' ),
+    sourcemaps      = require( 'gulp-sourcemaps' ),
+    livereload      = require( 'gulp-livereload' );
 
+let processors = [
+    autoprefixer({ overrideBrowserslist:[ 'last 2 version' ]})
+];
+
+const sassEntry = "public/vars/styles.sass";
 
 function styles(){
-    return src( "components/vars/styles.sass")
-       // .pipe(sourcemaps.init())
-       .pipe(sass())
-       // .pipe(sourcemaps.write())
-       .pipe(dest("components/"))
-       .pipe(livereload())
+    return src( sassEntry )
+       .pipe( sourcemaps.init() )
+       .pipe( sass() )
+       .pipe( sourcemaps.write() )
+       .pipe( dest("public/") )
+       .pipe( livereload() )
 }
 exports.styles = styles;
+
+const productionStyles = () =>
+   src( sassEntry )
+      .pipe( sass().on( 'error', sass.logError ) )
+      .pipe( postcss(processors) )
+      .pipe( gcmq() )
+      .pipe( csso() )
+      .pipe( dest( "public/" ) );
+
+exports.productionStyles = productionStyles;
 
 function startWatch() {
     livereload.listen();
