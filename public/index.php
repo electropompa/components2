39b35c777ee17af740e9da7e24203a77d4819cbd
Electropompa <admin@zilmet.ru>
2023-10-12 12:57:14 +0300

    separate php templates, add article list block

--- public/index.php
+++ public/index.php
@@ -1,16 +1,13 @@
+<?php
+$page_options = [
+    'title' => 'Компоненты - UI',
+];
+
+require "templates/menu.php";
+?>
 <!doctype html>
 <html lang="ru">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="styles.css">
-  <!-- <link rel="preconnect" href="//fonts.googleapis.com"> -->
-  <!-- <link rel="preconnect" href="//fonts.gstatic.com" crossorigin> -->
-  <!-- <link href="//fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&family=PT+Serif:wght@400&display=swap" rel="stylesheet"> -->
-  <title>Компоненты</title>
-</head>
-<body>
+<?php require "templates/header.php"; ?>
 
 <aside>
   <h4>Оглавление</h4>
@@ -489,71 +486,4 @@
   </table>
 </div>
 
-<h2 class="text-center my-5">Block Shift</h2>
-
-<section class="block-shift-img block-right-img-shift block-shift-img__outer-title">
-  <div class="container">
-    <div class="block-shift__grid">
-      <h3 class="block-shift__title">О дистрибьюторе</h3>
-      <div class="block-shift__desc">
-        <p>Поддержание полного ассортимента товаров и зап частей в наличии на собственном складском комплексе площадью более 1500 м<sup>2</sup> позволяет комплектовать заказы любой сложности в самые короткие сроки.</p>
-        <p>Региональным покупателям бесплатно доставим товар до терминалов транспортноых компаний в г. Москве.</p>
-        <p>Услуги гарантийного и постгарантийного обслужвания, ремонтные работы по замене зап. частей осуществляет Сервисная служба ГК Электропомпа.</p>
-      </div>
-      <div class="block-shift__img">
-        <img src="img-examples/sklad.jpg" alt="Склад ГК Электропомпа">
-      </div>
-    </div>
-  </div>
-</section>
-
-<section class="block-shift-img block-left-img-shift block-shift-img__outer-title">
-  <div class="container">
-    <div class="block-shift__grid">
-      <h3 class="block-shift__title">О дистрибьюторе</h3>
-      <div class="block-shift__desc">
-        <p>Поддержание полного ассортимента товаров и зап частей в наличии на собственном складском комплексе площадью более 1500 м<sup>2</sup> позволяет комплектовать заказы любой сложности в самые короткие сроки.</p>
-        <p>Региональным покупателям бесплатно доставим товар до терминалов транспортноых компаний в г. Москве.</p>
-        <p>Услуги гарантийного и постгарантийного обслужвания, ремонтные работы по замене зап. частей осуществляет Сервисная служба ГК Электропомпа.</p>
-      </div>
-      <div class="block-shift__img">
-        <img src="img-examples/sklad.jpg" alt="Склад ГК Электропомпа">
-      </div>
-    </div>
-  </div>
-</section>
-
-<section class="block-shift-img block-right-img-shift block-shift-img__inner-title">
-  <div class="container">
-    <div class="block-shift__grid">
-      <div class="block-shift__desc">
-        <h1 class="h3 block-shift__title">Сервисный центр</h1>
-        <p>По факту неисправности мембранного бака вы можете обратиться в наш Сервисный центр.</p>
-        <p>Мы выполняем сервисные работы по замене мембраны, фланца, покраске гидроаккумуляторов и т.д.</p>
-        <p>Вы всегда можете получить консультацию по вопросам гарантийного/пост гарантийного обслуживания, монтажа, эксплуатации оборудования, замене зап частей и т.д. у наших сервисных специалистов по тел. 8 800 100 00 77 в рабочее время.</p>
-      </div>
-      <div class="block-shift__img">
-        <img src="img-examples/service.webp" alt="Сервисный цент Zilmet">
-      </div>
-    </div>
-  </div>
-</section>
-
-<section class="block-shift-img block-left-img-shift block-shift-img__inner-title">
-  <div class="container">
-    <div class="block-shift__grid">
-      <div class="block-shift__desc">
-        <h1 class="h3 block-shift__title">Сервисный центр</h1>
-        <p>По факту неисправности мембранного бака вы можете обратиться в наш Сервисный центр.</p>
-        <p>Мы выполняем сервисные работы по замене мембраны, фланца, покраске гидроаккумуляторов и т.д.</p>
-        <p>Вы всегда можете получить консультацию по вопросам гарантийного/пост гарантийного обслуживания, монтажа, эксплуатации оборудования, замене зап частей и т.д. у наших сервисных специалистов по тел. 8 800 100 00 77 в рабочее время.</p>
-      </div>
-      <div class="block-shift__img">
-        <img src="img-examples/service.webp" alt="Сервисный цент Zilmet">
-      </div>
-    </div>
-  </div>
-</section>
-
-</body>
-</html>
+<?php require_once "templates/footer.php"; ?>
---
+++ public/templates/menu.php
@@ -0,0 +1,11 @@
+<?php
+function render_menu (){
+  return '
+    <nav role="navigation">
+      <ul class="list-inline">
+        <li><a href="/" title="UI Компоненты">UI Компоненты</a></li>
+        <li><a href="/blocks.php" title="Блоки">Блоки</a></li>
+      </ul>
+    </nav>
+  ';
+}
